c0251371ade50d674db647b5769514e49a347eb9
TomHavy <havy.to@gmail.comm>
2023-09-16 13:04:17 +0200

    added XGB model to models.py

--- app/core/models.py
+++ app/core/models.py
@@ -49,3 +49,20 @@ def rf_param_selector(seed):
 
     model = RandomForestClassifier(**params)
     return model
+
+import streamlit as st
+from xgboost import XGBClassifier
+
+def xgb_param_selector(seed):
+    n_estimators = st.number_input("n_estimators", 100, 1000, 100, 50)
+    learning_rate = st.number_input("learning_rate (%)", 1, 100, 10, 1)
+    max_depth = st.number_input("max_depth", 1, 10, 3, 1)
+
+    params = {
+        "n_estimators": n_estimators,
+        "learning_rate":learning_rate/100,
+        "max_depth": max_depth,
+    }
+    
+    model = XGBClassifier(**params)
+    return model
